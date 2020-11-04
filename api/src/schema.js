cf09508b74b61a69de5a5498b19a9d4f3db9f0ad
Paahn <pjhatzis@gmail.com>
2020-11-03 18:34:12 -0800

    added models in graphql schema

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,7 +4,17 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
+    Type User {
+        id: String!
+        username: String
+      }
   
+      Type Pet {
+        id: String!
+        createdAt: Number
+        name: String
+        type: String
+      }
 
 `;
 
