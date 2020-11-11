a3b526aeda11ca20f4fd4ab7420eee1d8fa1015e
Paahn <pjhatzis@gmail.com>
2020-11-11 14:29:52 -0800

    pet query returns only one pet
    based again on any parameter the client wants to pass in, since we are passing in the entire input object, it is all available to the client to query

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,7 +5,6 @@
 
 module.exports = {
   Query: {
-    pets(_, {type}, context) {
     pets(_, __, context) {
       return context.models.Pet.findMany()
     },
@@ -34,16 +33,8 @@ module.exports = {
         { type: 'Viking', size: 15, colour: 'Black', brand: 'Versace'}
       ].filter( brand => brand.brand === input.brand)
     },
-    pet(_, {input}) {
-      return [
-        { id: '33nD8', createdAt: '11-11-2020', name: 'Rotahn', type: 'Aligator'},
-        { id: '33nD9', createdAt: '10-11-2020', name: 'Dani', type: 'Mouse'},
-        { id: '33nD10', createdAt: '09-11-2020', name: 'Frell', type: 'Hermit Crab'},
-        { id: '33nD11', createdAt: '10-11-2020', name: 'Toto', type: 'Giraffe'},
-        { id: '33nD12', createdAt: '11-11-2020', name: 'Klari', type: 'Anaconda'},
-        { id: '33nD13', createdAt: '11-10-2020', name: 'Juke', type: 'Anteater'},
-        { id: '33nD14', createdAt: '01-11-2020', name: 'Olkorbab', type: 'Water Demon'}
-      ].filter( pet => pet.name === input.name)
+    pet(_, {input}, context) {
+      return context.models.Pet.findOne(input)
     }
   },
   // Mutation: {
